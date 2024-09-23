e05bb61ab3296c51533788cbacf21e77f901d4ca
Sanskriti <sanskritikhanal5@gmail.com>
2024-09-23 09:43:45 +0545

    test

--- Loops/Forloop.java
+++ Loops/Forloop.java
@@ -9,11 +9,21 @@ package Loops;
 
 public class Forloop {
     public static void main(String[] args) {
-        for (int i = 0; i < 99; i++) {
-            System.out.println("Hello world");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
 
+        }
+        /* print the numbers from 0 to 10 */
+        for (int count = 0; count < 11; count++) {
+            System.out.println(count);
         }
 
     }
 
 }
+
+/*
+ * initialisation = i=0
+ * terminting condition i<100
+ * updattion= i=i+1
+ */
---
+++ Loops/whileloop.java
@@ -0,0 +1,17 @@
+package Loops;
+
+/*while(condition){
+ * 
+ * //do something
+} */
+
+public class whileloop {
+    public static void main(String[] args) {
+        int i = 0;// initialisation
+        while (i < 100) { // condition
+            System.out.println(i);
+            i++;// updating
+
+        }
+    }
+}
