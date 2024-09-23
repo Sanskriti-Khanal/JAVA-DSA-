42c0ef711aca419487553c9a87c1d2c6d4f91687
Sanskriti <sanskritikhanal5@gmail.com>
2024-09-23 09:28:31 +0545

    Loops practive

---
+++ Loops/Forloop.java
@@ -0,0 +1,19 @@
+package Loops;
+
+/*Syntax
+ * for(initialisation;condition;update){
+ * 
+ *  //do something
+ * }
+ */
+
+public class Forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 99; i++) {
+            System.out.println("Hello world");
+
+        }
+
+    }
+
+}
