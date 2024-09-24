ba3cc683b19231d887826eb783753a18c598f251
Sanskriti <sanskritikhanal5@gmail.com>
2024-09-24 11:17:54 +0545

    Make hollow rectangle

---
+++ Pattern/hollowrect.java
@@ -0,0 +1,32 @@
+package Pattern;
+
+import java.util.*;
+
+public class hollowrect {
+    /*
+     * * * * * *
+     * * space *
+     * * space *
+     * * * * * *
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the desired number of rows: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the desired number of columns: ");
+        int m = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // cell - (i,j)
+                if (i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");// printing space between
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
