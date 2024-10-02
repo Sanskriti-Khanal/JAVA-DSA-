4ddd66e1b6aa29cd810691c38806056d8d472cba
Sanskriti <sanskritikhanal5@gmail.com>
2024-10-02 06:42:32 +0545

    Patterns With Number

--- Pattern/IHPnumbers.java
+++ Pattern/IHPnumbers.java
@@ -11,9 +11,8 @@ package Pattern;
 
 public class IHPnumbers {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i + 1; j++) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
             System.out.println();
